86ab226f251d3d5a24361abbfa1b12d1b9b97f23
Autumn781 <chenziheng935733@163.com>
2025-03-05 13:06:14 +0800

    Add files via upload

---
+++ 4.1 定义一维数组3.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+    int i,j,t,a[10];
+    printf("shuru:\n");
+    for(i=0;i<=9;i++)
+      scanf("%d",&a[i]);
+    for(j=0;j<=8;j++)
+      for(i=0;i<=8-j;i++)
+       if(a[i]>a[i+1])
+       {
+        t=a[i];
+        a[i]=a[i+1];
+        a[i+1]=t;
+       }
+    printf("paixvhou\n");
+    for(i=0;i<=9;i++)
+        printf("%d\t",a[i]);
+    printf("\n");
+
+}
